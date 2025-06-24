3ad11165dca8608ebea9aecd1ff35de9ba9a89f2
Diego Gomez <dgomez@tenstorrent.com>
2025-06-24 12:46:07 -0400

    improving the tests

--- ttnn/cpp/ttnn/operations/experimental/experimental_pybind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_pybind.cpp
@@ -47,7 +47,8 @@
 #include "ttnn/operations/experimental/scatter/tosa_scatter_pybind.hpp"
 #include "ttnn/operations/experimental/padded_slice/padded_slice_pybind.hpp"
 #include "ttnn/operations/experimental/where/where_pybind.hpp"
-#include "ttnn/operations/experimental/test/hang_operation_pybind.hpp"
+#include "ttnn/operations/experimental/test/hang_host/hang_host_operation_pybind.hpp"
+#include "ttnn/operations/experimental/test/hang_device/hang_device_operation_pybind.hpp"
 namespace py = pybind11;
 
 namespace ttnn::operations::experimental {
@@ -112,7 +113,8 @@ void py_module(py::module& module) {
 
     tosa::gather::detail::bind_gather_tosa_operation(module);
 
-    test::bind_test_hang_operation(module);
+    test::bind_test_hang_host_operation(module);
+    test::bind_test_hang_device_operation(module);
     // CCL ops
     auto m_experimental_ccl =
         module.def_submodule("ccl_experimental", "experimental collective communication operations");

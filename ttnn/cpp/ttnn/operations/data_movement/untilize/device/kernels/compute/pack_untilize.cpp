f7034647880872a34f482f3f86f9512738c7324d
ppopovic <ppopovic@tenstorrent.com>
2025-06-27 16:17:59 +0000

    #0: Untilize fix

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/compute/pack_untilize.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/compute/pack_untilize.cpp
@@ -24,10 +24,10 @@ void MAIN {
 
         pack_untilize_block<per_core_block_tile_cnt>(src_cb_id, 1, out_cb_id);
 
-        // cb_push_back(out_cb_id, per_core_block_tile_cnt);
+        cb_push_back(out_cb_id, per_core_block_tile_cnt);
         // cb_pop_front(src_cb_id, per_core_block_tile_cnt);
     }
-    cb_push_back(out_cb_id, per_core_block_tile_cnt * per_core_block_cnt);
+    //    cb_push_back(out_cb_id, per_core_block_tile_cnt * per_core_block_cnt);
     cb_pop_front(src_cb_id, per_core_block_tile_cnt * per_core_block_cnt);
 
     pack_untilize_uninit(out_cb_id);

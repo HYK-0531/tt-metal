0c823286fe156e37c171ad9bd33a66a273db17d7
Almeet Bhullar <abhullar@tenstorrent.com>
2025-06-30 17:10:18 +0000

    Make dram arbiter hang reproducible

--- tests/tt_metal/tt_metal/test_kernels/dataflow/dram_arbiter_hang.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/dram_arbiter_hang.cpp
@@ -9,7 +9,7 @@ void kernel_main() {
     uint32_t page_size = get_arg_val<uint32_t>(1);
     uint32_t dst_l1_addr = get_arg_val<uint32_t>(2);
     uint32_t num_pages_to_read = get_arg_val<uint32_t>(3);
-    uint32_t num_iterations = get_arg_val<uint32_t>(4);
+    uint32_t num_iterations = 100000;  // get_arg_val<uint32_t>(4);
 
     const InterleavedAddrGenFast<true> s = {
         .bank_base_address = bank_base_address, .page_size = page_size, .data_format = DataFormat::Float16_b};

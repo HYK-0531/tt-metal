7d1cb2842a949e0435e1b04bcdd176e49b3aea3e
Colman Glagovich <cglagovich@tenstorrent.com>
2025-07-02 13:42:25 +0000

    Clean up forward/backwards fabric calls and use constexpr on direction

--- ttnn/cpp/ttnn/operations/experimental/ccl/reduce_scatter_minimal_async/device/kernels/minimal_ccl_common.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/reduce_scatter_minimal_async/device/kernels/minimal_ccl_common.hpp
@@ -94,3 +94,44 @@ FORCE_INLINE void write_and_advance_local_read_address_for_fabric_write_backward
 
     l1_read_addr += payload_size_bytes;
 }
+
+FORCE_INLINE void scatter_write_and_advance_local_read_address_for_fabric(
+    uint64_t first_noc0_dest_noc_addr,
+    uint64_t second_noc0_dest_noc_addr,
+    volatile PACKET_HEADER_TYPE* pkt_hdr,
+    tt::tt_fabric::WorkerToFabricEdmSender* fabric_direction_connection,
+    size_t& l1_read_addr,
+    uint32_t first_payload_size_bytes,
+    uint32_t second_payload_size_bytes) {
+    pkt_hdr->to_noc_unicast_scatter_write(
+        tt::tt_fabric::NocUnicastScatterCommandHeader{
+            {first_noc0_dest_noc_addr, second_noc0_dest_noc_addr}, (uint16_t)first_payload_size_bytes},
+        first_payload_size_bytes + second_payload_size_bytes);
+
+    fabric_direction_connection->wait_for_empty_write_slot();
+    fabric_direction_connection->send_payload_without_header_non_blocking_from_address(
+        l1_read_addr, first_payload_size_bytes + second_payload_size_bytes);
+    fabric_direction_connection->send_payload_flush_non_blocking_from_address(
+        (uint32_t)pkt_hdr, sizeof(PACKET_HEADER_TYPE));
+    noc_async_writes_flushed();
+
+    l1_read_addr += first_payload_size_bytes + second_payload_size_bytes;
+}
+
+FORCE_INLINE void write_and_advance_local_read_address_for_fabric(
+    uint64_t noc0_dest_noc_addr,
+    volatile PACKET_HEADER_TYPE* pkt_hdr,
+    tt::tt_fabric::WorkerToFabricEdmSender* fabric_direction_connection,
+    size_t& l1_read_addr,
+    uint32_t payload_size_bytes) {
+    pkt_hdr->to_noc_unicast_write(tt::tt_fabric::NocUnicastCommandHeader{noc0_dest_noc_addr}, payload_size_bytes);
+
+    fabric_direction_connection->wait_for_empty_write_slot();
+    fabric_direction_connection->send_payload_without_header_non_blocking_from_address(
+        l1_read_addr, payload_size_bytes);
+    fabric_direction_connection->send_payload_flush_non_blocking_from_address(
+        (uint32_t)pkt_hdr, sizeof(PACKET_HEADER_TYPE));
+    noc_async_writes_flushed();
+
+    l1_read_addr += payload_size_bytes;
+}

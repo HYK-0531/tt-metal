c94f33d219567e5178ebb2173f10c54da32417f0
Anil Mahmud <amahmud@tenstorrent.com>
2025-07-04 01:28:29 +0000

    Increase repro rate

--- tests/ttnn/unit_tests/operations/test_examples.py
+++ tests/ttnn/unit_tests/operations/test_examples.py
@@ -20,24 +20,34 @@ def test_example(device):
 
     expected_output = torch_input * torch_other
 
-    # Starting from zero, simulator repros error at 19/25
-    for x in range(1000):
-        print("iter", x)
-        print("nops", x)
-        os.environ["UNOPS"] = str(x)
-        tt_input = ttnn.from_torch(torch_input, layout=ttnn.TILE_LAYOUT, device=device)
-        tt_other = ttnn.from_torch(torch_other, layout=ttnn.TILE_LAYOUT, device=device)
+    # There is a cumulative effect, if we run the two kernels inside the inner loop multiple times
+    # at a certain iteration, which will introduce a specific number of nops at the top of the second
+    # kernel's unpacker code, the output will be wrong. If we just run one iteration with that many
+    # nops on top of the unpacker code, it will not fault. The number of nops required ranges between 19-28.
+    # Starting from 17 empirically optimizes the probability of encountering the error.
+    max_nops_required = 28
+    optimal_start_point = 17
+    iteration_count = 0
+    for y in range(optimal_start_point, max_nops_required):
+        for x in range(y, max_nops_required):
+            print("iteration_count ", iteration_count)
+            print("nops ", x)
+            os.environ["UNOPS"] = str(x)
+            tt_input = ttnn.from_torch(torch_input, layout=ttnn.TILE_LAYOUT, device=device)
+            tt_other = ttnn.from_torch(torch_other, layout=ttnn.TILE_LAYOUT, device=device)
 
-        tt_output1 = ttnn.empty(shape, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
-        tt_output = ttnn.empty(shape, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+            tt_output1 = ttnn.empty(shape, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+            tt_output = ttnn.empty(shape, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
 
-        ttnn.prim.example(tt_other, tt_output1)
-        ttnn.prim.example_multiple_return(tt_input, tt_output1, output=tt_output)
+            ttnn.prim.example(tt_other, tt_output1)
+            ttnn.prim.example_multiple_return(tt_input, tt_output1, output=tt_output)
 
-        actual_output = ttnn.to_torch(tt_output)
+            actual_output = ttnn.to_torch(tt_output)
 
-        ret = torch.allclose(actual_output, expected_output, atol=0, rtol=0)
-        if not ret:
-            print("actual_output", actual_output.to(torch.float32).flatten().tolist())
-            print("FAILED")
-            break
+            ret = torch.allclose(actual_output, expected_output, atol=0, rtol=0)
+            if not ret:
+                print("FAILED")
+                print("actual_output", actual_output.to(torch.float32).flatten().tolist())
+                return
+
+            iteration_count = iteration_count + 1

8e384217dfca36967850934b112faf5f6b53ba9e
Radoica Draskic <rdraskic@tenstorrent.com>
2025-07-09 14:14:07 +0000

    Fix dispatch core config

--- ttnn/ttnn/distributed/distributed.py
+++ ttnn/ttnn/distributed/distributed.py
@@ -164,7 +164,7 @@ def open_mesh_device(
         l1_small_size=l1_small_size,
         trace_region_size=trace_region_size,
         num_command_queues=num_command_queues,
-        dispatch_core_config=dispatch_core_config,
+        dispatch_core_config=ttnn.DispatchCoreConfig(),
         offset=offset,
         physical_device_ids=physical_device_ids,
         worker_l1_size=worker_l1_size,

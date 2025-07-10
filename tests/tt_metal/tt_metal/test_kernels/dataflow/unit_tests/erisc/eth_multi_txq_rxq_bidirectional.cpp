9143567cd045f7239292ba7050bb19ddc7e7a49d
Sean Nijjar <sean.nijjar@gmail.com>
2025-07-10 03:03:48 +0000

    add missing licensing headers

--- tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/eth_multi_txq_rxq_bidirectional.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/eth_multi_txq_rxq_bidirectional.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
 
 #include "risc_common.h"
 #include "tt_metal/fabric/hw/inc/edm_fabric/edm_handshake.hpp"

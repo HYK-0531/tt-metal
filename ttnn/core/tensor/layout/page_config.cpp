8dc9c69578195f1f4e0163a28b33667d8aac9942
Stanislav Minakov <sminakov@tenstorrent.com>
2025-07-10 23:04:20 -0700

    Update alignment calculations

--- ttnn/core/tensor/layout/page_config.cpp
+++ ttnn/core/tensor/layout/page_config.cpp
@@ -84,13 +84,17 @@ Tile PageConfig::get_tile() const {
 TilePageConfig::TilePageConfig(const Tile& tile) : tile_(tile) {}
 
 Alignment TilePageConfig::create_default_alignment(DataType dtype, const MemoryConfig& memory_config) const {
+    uint32_t height_alignment = tile_.get_height();
+    uint32_t width_alignment = tile_.get_width();
     if (memory_config.shard_spec().has_value()) {
         const auto& shard_spec = memory_config.shard_spec().value();
-        if (shard_spec.physical_shard_shape.has_value()) {
-            return Alignment(shard_spec.physical_shard_shape.value());
-        }
+        auto shard_shape = shard_spec.physical_shard_shape.value_or(shard_spec.shape);
+        width_alignment = std::lcm(width_alignment, shard_shape[1]);
+    } else if (memory_config.nd_shard_spec().has_value()) {
+        const auto& nd_shard_spec = *memory_config.nd_shard_spec();
+        width_alignment = std::lcm(width_alignment, nd_shard_spec.shard_shape[-1]);
     }
-    return Alignment({tile_.get_height(), tile_.get_width()});
+    return Alignment({height_alignment, width_alignment});
 }
 
 void TilePageConfig::validate_alignment(const Alignment& alignment, DataType dtype, const MemoryConfig&) const {
@@ -130,22 +134,16 @@ const Tile& TilePageConfig::get_tile() const { return tile_; }
 RowMajorPageConfig::RowMajorPageConfig(const Tile& tile) : tile_(tile) {}
 
 Alignment RowMajorPageConfig::create_default_alignment(DataType dtype, const MemoryConfig& memory_config) const {
+    uint32_t width_alignment = 1;
     if (memory_config.shard_spec().has_value()) {
         const auto& shard_spec = memory_config.shard_spec().value();
-        if (shard_spec.mode == ShardMode::LOGICAL) {
-            return shard_spec.physical_shard_shape.has_value() ? Alignment(shard_spec.physical_shard_shape.value())
-                                                               : Alignment({shard_spec.shape[1]});
-        }
-        // TODO: Investigate why we need guard against HEIGHT_SHARDED and merge logic with LOGICAL sharding
-        if (shard_spec.mode == ShardMode::PHYSICAL &&
-            memory_config.memory_layout() != TensorMemoryLayout::HEIGHT_SHARDED) {
-            return Alignment({shard_spec.shape[1]});
-        }
+        auto shard_shape = shard_spec.physical_shard_shape.value_or(shard_spec.shape);
+        width_alignment = shard_shape[1];
     } else if (memory_config.nd_shard_spec().has_value()) {
         const auto& nd_shard_spec = *memory_config.nd_shard_spec();
-        return Alignment({nd_shard_spec.shard_shape[-1]});
+        width_alignment = nd_shard_spec.shard_shape[-1];
     }
-    return Alignment({1});
+    return Alignment({width_alignment});
 }
 
 void RowMajorPageConfig::validate_alignment(

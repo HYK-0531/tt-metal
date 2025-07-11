e9f1b867736139bfddf509f1b76571d3eb80e0d7
Rico Zhu <ricozhu@tenstorrent.com>
2025-07-11 18:51:27 +0000

    Last commit for now

--- models/experimental/minimax/tests/test_moe.py
+++ models/experimental/minimax/tests/test_moe.py
@@ -7,32 +7,9 @@ from safetensors.torch import load_file
 from models.experimental.minimax.tt.minimax_moe import TTMiniMaxM1SparseMoeBlock
 from models.experimental.minimax.tt.minimax_config import TTMiniMaxM1MoeConfig
 
-from .test_utils import get_minimax_tokenizer_and_config_and_state_dict
-
 
 @pytest.fixture
 def setup():
-    torch.manual_seed(1234)
-    device = ttnn.open_device(device_id=0)
-    tokenizer, config, state_dict = get_minimax_tokenizer_and_config_and_state_dict()
-    return tokenizer, config, state_dict, device
-
-
-def test_moe_load_weights(setup):
-    _, config, state_dict, device = setup
-    # Test block_sparse_moe
-    test_base_address = f"model.layers.0.block_sparse_moe"
-    block_sparse_moe = TTMiniMaxM1SparseMoeBlock(config, state_dict, test_base_address, device=device)
-
-
-def test_moe_inference(setup):
-    _, config, state_dict, device = setup
-    # Test block_sparse_moe
-    test_base_address = f"model.layers.0.block_sparse_moe"
-    block_sparse_moe = TTMiniMaxM1SparseMoeBlock(config, state_dict, test_base_address, device=device)
-
-
-def test_moe_sharded():
     torch.manual_seed(1234)
     device = ttnn.open_device(device_id=0)
 
@@ -49,3 +26,9 @@ def test_moe_sharded():
     # Test block_sparse_moe
     test_base_address = f"model.layers.0.block_sparse_moe"
     block_sparse_moe = TTMiniMaxM1SparseMoeBlock(config, state_dict_shard, test_base_address, device=device)
+    return block_sparse_moe
+
+
+def test_moe_inference(setup):
+    block_sparse_moe = setup
+    # Generate a random sequence to use

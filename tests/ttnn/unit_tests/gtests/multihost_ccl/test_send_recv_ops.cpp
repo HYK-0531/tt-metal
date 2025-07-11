b61ca098c1aa486ad28652038aac07d9397c38f9
Austin Ho <aho@tenstorrent.com>
2025-07-11 22:22:38 +0000

    #0: Generalize send receive on mixed buffer types of tensor and socket

--- tests/ttnn/unit_tests/gtests/multihost_ccl/test_send_recv_ops.cpp
+++ tests/ttnn/unit_tests/gtests/multihost_ccl/test_send_recv_ops.cpp
@@ -14,50 +14,38 @@
 #include "tt_metal/multihost/fabric_tests/multihost_fabric_fixtures.hpp"
 #include <tt-metalium/mesh_socket.hpp>
 #include <tt-metalium/distributed_context.hpp>
+#include "ccl/send_recv_op_utils.hpp"
 
 namespace tt::tt_metal {
 
-std::array<TensorSpec, 2> tensor_specs = {
-    TensorSpec(
-        ttnn::Shape({3, 2, 32, 128}),
-        tt::tt_metal::TensorLayout(
-            tt::tt_metal::DataType::UINT32,
-            tt::tt_metal::PageConfig(tt::tt_metal::Layout::ROW_MAJOR),
-            tt::tt_metal::MemoryConfig(tt::tt_metal::TensorMemoryLayout::INTERLEAVED, tt::tt_metal::BufferType::DRAM))),
-    TensorSpec(
-        ttnn::Shape({3, 2, 32, 128}),
-        tt::tt_metal::TensorLayout(
-            tt::tt_metal::DataType::UINT32,
-            tt::tt_metal::PageConfig(tt::tt_metal::Layout::TILE),
-            tt::tt_metal::MemoryConfig(tt::tt_metal::TensorMemoryLayout::INTERLEAVED, tt::tt_metal::BufferType::DRAM))),
-};
-
 class MeshDeviceDual2x4SendRecvFixture : public tt::tt_fabric::fabric_router_tests::MeshDeviceDual2x4Fixture,
-                                         public testing::WithParamInterface<TensorSpec> {};
+                                         public testing::WithParamInterface<std::tuple<TensorSpec, BufferType>> {};
 
 INSTANTIATE_TEST_SUITE_P(
     MeshDeviceDual2x4SendRecvTests, MeshDeviceDual2x4SendRecvFixture, ::testing::ValuesIn(tensor_specs));
 
 class MeshDeviceSplit2x2SendRecvFixture : public tt::tt_fabric::fabric_router_tests::MeshDeviceSplit2x2Fixture,
-                                          public testing::WithParamInterface<TensorSpec> {};
+                                          public testing::WithParamInterface<std::tuple<TensorSpec, BufferType>> {};
 
 INSTANTIATE_TEST_SUITE_P(
     MeshDeviceSplit2x2SendRecvTests, MeshDeviceSplit2x2SendRecvFixture, ::testing::ValuesIn(tensor_specs));
 
 class MeshDeviceSplit1x2SendRecvFixture : public tt::tt_fabric::fabric_router_tests::MeshDeviceSplit1x2Fixture,
-                                          public testing::WithParamInterface<TensorSpec> {};
+                                          public testing::WithParamInterface<std::tuple<TensorSpec, BufferType>> {};
 
 INSTANTIATE_TEST_SUITE_P(
     MeshDeviceSplit1x2SendRecvTests, MeshDeviceSplit1x2SendRecvFixture, ::testing::ValuesIn(tensor_specs));
 
 class MeshDeviceNanoExaboxSendRecvFixture : public tt::tt_fabric::fabric_router_tests::MeshDeviceNanoExaboxFixture,
-                                            public testing::WithParamInterface<TensorSpec> {};
+                                            public testing::WithParamInterface<std::tuple<TensorSpec, BufferType>> {};
 INSTANTIATE_TEST_SUITE_P(
     MeshDeviceNanoExaboxSendRecvTests, MeshDeviceNanoExaboxSendRecvFixture, ::testing::ValuesIn(tensor_specs));
 
-void test_send_recv_async(
+template <typename T>
+void test_send_recv_async_(
     const std::shared_ptr<tt::tt_metal::distributed::MeshDevice>& mesh_device,
     const TensorSpec& tensor_spec,
+    BufferType socket_buffer_type,
     distributed::multihost::Rank sender_rank,
     distributed::multihost::Rank receiver_rank,
     uint32_t seed) {
@@ -82,7 +70,7 @@ void test_send_recv_async(
     }
 
     distributed::SocketMemoryConfig socket_mem_config = {
-        .socket_storage_type = BufferType::L1,
+        .socket_storage_type = socket_buffer_type,
         .fifo_size = socket_fifo_size,
     };
 
@@ -110,16 +98,15 @@ void test_send_recv_async(
             ttnn::distributed::distribute_tensor(
                 ttnn::experimental::view(ttnn::arange(seed, seed + num_elems, 1, dtype), input_shape).to_layout(layout),
                 *ttnn::distributed::replicate_tensor_to_mesh_mapper(*mesh_device),
-                *mesh_device)
+                std::nullopt)
                 .to_device(mesh_device.get(), memory_config);
         ttnn::experimental::send_async(input_tensor, forward_socket);
         distributed::Synchronize(mesh_device.get(), std::nullopt);
         auto composer = ttnn::distributed::concat_mesh_to_tensor_composer(*mesh_device, /*dim=*/0);
-        auto input_data = ttnn::distributed::aggregate_tensor(input_tensor, *composer).to_vector<uint32_t>();
+        auto input_data = ttnn::distributed::aggregate_tensor(input_tensor, *composer).to_vector<T>();
         // Send test results to the receiver host
         distributed_context->send(
-            tt::stl::Span<std::byte>(
-                reinterpret_cast<std::byte*>(input_data.data()), input_data.size() * sizeof(uint32_t)),
+            tt::stl::Span<std::byte>(reinterpret_cast<std::byte*>(input_data.data()), input_data.size() * sizeof(T)),
             receiver_rank,  // send to receiver host
             tag             // exchange test results over tag 0
         );
@@ -128,11 +115,11 @@ void test_send_recv_async(
             mesh_device.get());
         ttnn::experimental::recv_async(output_tensor, backward_socket);
         distributed::Synchronize(mesh_device.get(), std::nullopt);
-        auto output_data = ttnn::distributed::aggregate_tensor(output_tensor, *composer).to_vector<uint32_t>();
-        std::vector<uint32_t> inc_output_data(output_data.size());
+        auto output_data = ttnn::distributed::aggregate_tensor(output_tensor, *composer).to_vector<T>();
+        std::vector<T> inc_output_data(output_data.size());
         distributed_context->recv(
             tt::stl::Span<std::byte>(
-                reinterpret_cast<std::byte*>(inc_output_data.data()), inc_output_data.size() * sizeof(uint32_t)),
+                reinterpret_cast<std::byte*>(inc_output_data.data()), inc_output_data.size() * sizeof(T)),
             receiver_rank,  // recv from receiver host
             tag             // exchange test results over tag 0
         );
@@ -144,11 +131,10 @@ void test_send_recv_async(
         ttnn::experimental::recv_async(output_tensor, forward_socket);
         distributed::Synchronize(mesh_device.get(), std::nullopt);
         auto composer = ttnn::distributed::concat_mesh_to_tensor_composer(*mesh_device, /*dim=*/0);
-        auto output_data = ttnn::distributed::aggregate_tensor(output_tensor, *composer).to_vector<uint32_t>();
-        std::vector<uint32_t> input_data(output_data.size());
+        auto output_data = ttnn::distributed::aggregate_tensor(output_tensor, *composer).to_vector<T>();
+        std::vector<T> input_data(output_data.size());
         distributed_context->recv(
-            tt::stl::Span<std::byte>(
-                reinterpret_cast<std::byte*>(input_data.data()), input_data.size() * sizeof(uint32_t)),
+            tt::stl::Span<std::byte>(reinterpret_cast<std::byte*>(input_data.data()), input_data.size() * sizeof(T)),
             sender_rank,  // recv from sender host
             tag           // exchange test results over tag 0
         );
@@ -156,59 +142,92 @@ void test_send_recv_async(
         auto inc_output_tensor = ttnn::add(output_tensor, 1);
         ttnn::experimental::send_async(inc_output_tensor, backward_socket);
         distributed::Synchronize(mesh_device.get(), std::nullopt);
-        auto inc_output_data = ttnn::distributed::aggregate_tensor(inc_output_tensor, *composer).to_vector<uint32_t>();
+        auto inc_output_data = ttnn::distributed::aggregate_tensor(inc_output_tensor, *composer).to_vector<T>();
         distributed_context->send(
             tt::stl::Span<std::byte>(
-                reinterpret_cast<std::byte*>(inc_output_data.data()), inc_output_data.size() * sizeof(uint32_t)),
+                reinterpret_cast<std::byte*>(inc_output_data.data()), inc_output_data.size() * sizeof(T)),
             sender_rank,  // send to sender host
             tag           // exchange test results over tag 0
         );
     }
 }
 
+void test_send_recv_async(
+    const std::shared_ptr<tt::tt_metal::distributed::MeshDevice>& mesh_device,
+    const TensorSpec& tensor_spec,
+    BufferType socket_buffer_type,
+    distributed::multihost::Rank sender_rank,
+    distributed::multihost::Rank receiver_rank,
+    uint32_t seed) {
+    switch (tensor_spec.data_type()) {
+        case tt::tt_metal::DataType::BFLOAT16:
+            test_send_recv_async_<bfloat16>(
+                mesh_device, tensor_spec, socket_buffer_type, sender_rank, receiver_rank, seed);
+            break;
+        case tt::tt_metal::DataType::UINT32:
+            test_send_recv_async_<uint32_t>(
+                mesh_device, tensor_spec, socket_buffer_type, sender_rank, receiver_rank, seed);
+            break;
+        default: GTEST_SKIP() << "Unsupported data type: " << tensor_spec.data_type(); break;
+    }
+}
+
 TEST_P(MeshDeviceDual2x4SendRecvFixture, SendRecvAsync) {
-    auto tensor_spec = GetParam();
+    auto [tensor_spec, socket_buffer_type] = GetParam();
     test_send_recv_async(
-        mesh_device_, tensor_spec, distributed::multihost::Rank{0}, distributed::multihost::Rank{1}, 0);
+        mesh_device_,
+        tensor_spec,
+        socket_buffer_type,
+        distributed::multihost::Rank{0},
+        distributed::multihost::Rank{1},
+        0);
 }
 
 TEST_P(MeshDeviceSplit2x2SendRecvFixture, SendRecvAsync) {
-    auto tensor_spec = GetParam();
+    auto [tensor_spec, socket_buffer_type] = GetParam();
     test_send_recv_async(
-        mesh_device_, tensor_spec, distributed::multihost::Rank{0}, distributed::multihost::Rank{1}, 0);
+        mesh_device_,
+        tensor_spec,
+        socket_buffer_type,
+        distributed::multihost::Rank{0},
+        distributed::multihost::Rank{1},
+        0);
 }
 
 TEST_P(MeshDeviceSplit1x2SendRecvFixture, MultiSendRecvAsync) {
     constexpr distributed::multihost::Rank receiver_rank = distributed::multihost::Rank{0};
-    auto tensor_spec = GetParam();
+    auto [tensor_spec, socket_buffer_type] = GetParam();
     auto distributed_context = tt_metal::distributed::multihost::DistributedContext::get_current_world();
     auto rank = *(distributed_context->rank());
-    for (uint32_t i = 0; i < 100; i++) {
+    for (uint32_t i = 0; i < 10; i++) {
         if (rank == *receiver_rank || rank == 1) {
-            test_send_recv_async(mesh_device_, tensor_spec, distributed::multihost::Rank{1}, receiver_rank, i);
+            test_send_recv_async(
+                mesh_device_, tensor_spec, socket_buffer_type, distributed::multihost::Rank{1}, receiver_rank, i);
         }
         // TODO: Support routing through other meshes
         // if (rank == *receiver_rank || rank == 2) {
         //     test_send_recv_async(mesh_device_, tensor_spec, distributed::multihost::Rank{2}, receiver_rank, i);
         // }
         if (rank == *receiver_rank || rank == 3) {
-            test_send_recv_async(mesh_device_, tensor_spec, distributed::multihost::Rank{3}, receiver_rank, i);
+            test_send_recv_async(
+                mesh_device_, tensor_spec, socket_buffer_type, distributed::multihost::Rank{3}, receiver_rank, i);
         }
     }
 }
 
 TEST_P(MeshDeviceNanoExaboxSendRecvFixture, MultiSendRecvAsync) {
     constexpr distributed::multihost::Rank receiver_rank = distributed::multihost::Rank{1};
-    auto tensor_spec = GetParam();
+    auto [tensor_spec, socket_buffer_type] = GetParam();
     auto distributed_context = tt_metal::distributed::multihost::DistributedContext::get_current_world();
     auto rank = *(distributed_context->rank());
-    for (uint32_t i = 0; i < 100; i++) {
+    for (uint32_t i = 0; i < 10; i++) {
         for (uint32_t r = 0; r < *(distributed_context->size()); r++) {
             if (r == *receiver_rank) {
                 continue;
             }
             if (rank == *receiver_rank || rank == r) {
-                test_send_recv_async(mesh_device_, tensor_spec, distributed::multihost::Rank{r}, receiver_rank, i);
+                test_send_recv_async(
+                    mesh_device_, tensor_spec, socket_buffer_type, distributed::multihost::Rank{r}, receiver_rank, i);
             }
         }
     }

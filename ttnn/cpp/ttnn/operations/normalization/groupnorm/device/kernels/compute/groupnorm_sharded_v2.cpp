d0a7b96c41bc307ba06280763957254633ae384d
vsureshTT <vsuresh@tenstorrent.com>
2025-07-14 19:25:56 +0000

    saving, revert later

--- ttnn/cpp/ttnn/operations/normalization/groupnorm/device/kernels/compute/groupnorm_sharded_v2.cpp
+++ ttnn/cpp/ttnn/operations/normalization/groupnorm/device/kernels/compute/groupnorm_sharded_v2.cpp
@@ -149,6 +149,9 @@ void MAIN {
 #ifdef READER_REPACK
         cb_wait_front(cb_in_rm, per_core_N);
 #endif
+        for (uint32_t i = 0; i < per_core_MN; i++) {
+            UNPACK(tt::compute::common::print_full_tile(cb_in_rm, i, true));
+        }
         cb_reserve_back(cb_in, per_core_N);
         tilize_block(cb_in_rm, per_core_N, cb_in);
         cb_pop_front(cb_in_rm, per_core_N);
@@ -156,9 +159,13 @@ void MAIN {
     }
     tilize_uninit(cb_in_rm, cb_in);
     cb_wait_front(cb_in, per_core_MN);
-    for (uint32_t i = 0; i < per_core_MN; i++) {
-        UNPACK(tt::compute::common::print_full_tile(cb_in, i, true));
-    }
+    // UNPACK(DPRINT << "cb_in_rm: "<<cb_in_rm << ENDL());
+    // UNPACK(DPRINT << "cb_in0: "<<cb_in0 << ENDL());
+    // UNPACK(DPRINT << "per_core_N: "<<per_core_N << ENDL());
+    // UNPACK(DPRINT << "per_core_NM: "<<per_core_MN << ENDL());
+    // for(uint32_t i = 0; i < per_core_MN; i++){
+    //     UNPACK(tt::compute::common::print_full_tile(cb_in, i, true));
+    // }
 #else
     binary_op_init_common(cb_in0, cb_input_mask, cb_x);
 #endif

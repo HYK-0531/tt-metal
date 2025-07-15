1c8812604af57ec87854274b4cc5a01cb1906a84
Djordje <divanovic@tenstorrent.com>
2025-07-15 13:52:12 +0000

    #0: Add support for 80L run in APC

--- models/demos/llama3_subdevices/demo/text_demo.py
+++ models/demos/llama3_subdevices/demo/text_demo.py
@@ -86,6 +86,26 @@ def load_inputs(user_input, len_per_batch, instruct):
     return in_prompt
 
 
+def load_demo_targets(filename, galaxy_type):
+    """
+    Load expected demo targets from a JSON file.
+    """
+    if not os.path.exists(filename):
+        logger.warning(f"Expected outputs file {filename} does not exist. Skipping loading targets.")
+        return []
+
+    with open(filename, "r") as f:
+        try:
+            demo_targets = json.load(f)
+        except json.JSONDecodeError as e:
+            logger.error(f"Error decoding JSON from {filename}: {e}. Returning empty list.")
+            return []
+
+    demo_targets = demo_targets["targets"][galaxy_type]
+
+    return demo_targets
+
+
 def create_tt_model(
     mesh_device,
     instruct,
@@ -165,7 +185,7 @@ def create_tt_model(
 # optimization (LlamaOptimizations): Optimization level to use for the model (performance or accuracy)
 # MESH_DEVICE (str): Fake device to use for testing (N150, N300, T3K, TG). Usage: `export MESH_DEVICE=N150`, will enable running a single-chip demo on a multi-chip system.
 @pytest.mark.parametrize(
-    "input_prompts, instruct, repeat_batches, max_seq_len, batch_size, max_generated_tokens, paged_attention, page_params, sampling_params, stop_at_eos, ci_only, pcc_check, num_layers, run_on_6U",
+    "input_prompts, instruct, repeat_batches, max_seq_len, batch_size, max_generated_tokens, paged_attention, page_params, sampling_params, stop_at_eos, apc_test, pcc_check, num_layers, run_on_6U",
     [
         (  # Batch-32 run (Throughput) - 32 users, small prompt
             "models/demos/llama3_subdevices/demo/input_data_questions_prefill_128.json",  # input_prompts
@@ -178,7 +198,7 @@ def create_tt_model(
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only
+            False,  # apc_test
             False,  # pcc_check
             80,  # num layers
             True,  # run on 6U
@@ -194,7 +214,7 @@ def create_tt_model(
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only
+            False,  # apc_test
             False,  # pcc_check
             80,  # num layers
             True,  # run on 6U
@@ -210,7 +230,7 @@ def create_tt_model(
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only,
+            False,  # apc_test,
             False,  # pcc_check
             80,  # num layers
             True,  # run on 6U
@@ -226,7 +246,7 @@ def create_tt_model(
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only,
+            False,  # apc_test,
             False,  # pcc_check
             80,  # num layers
             True,  # run on 6U
@@ -242,25 +262,25 @@ def create_tt_model(
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only,
+            False,  # apc_test,
             False,  # pcc_check
             80,  # num layers
             False,  # run on 6U
         ),
-        (  # CI Run for PCC check for 1 Layer: Batch-32 run (Throughput) - 32 users, prompt is "This is a test"
+        (  # APC Run for PCC check, perf and functionality check: Batch-32 run (Throughput) - 32 users, prompt is "This is a test"
             "models/demos/llama3_subdevices/demo/input_data_questions_reference.json",  # input_prompts
-            False,  # instruct mode
+            True,  # instruct mode
             1,  # repeat_batches
             128 * 1024,  # max_seq_len
             32,  # batch_size
-            5,  # max_generated_tokens
+            130,  # max_generated_tokens
             True,  # paged_attention
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only
+            True,  # apc_test
             True,  # pcc_check
-            1,  # num layers
+            80,  # num layers
             True,  # run on 6U
         ),
         (  # CI Run for PCC check for 80 Layers + Teacher Forced: Batch-32 run (Throughput) - 32 users, prompt is "This is a test"
@@ -274,7 +294,7 @@ def create_tt_model(
             {"page_block_size": 64, "page_max_num_blocks": 2048},  # page_params
             {"temperature": 0, "top_p": 0.08},  # sampling_params (argmax)
             True,  # stop_at_eos
-            False,  # ci_only
+            False,  # apc_test
             True,  # pcc_check
             80,  # num layers
             True,  # run on 6U
@@ -286,7 +306,7 @@ def create_tt_model(
         "repeat2",  # latency with 5 repeat batches
         "long-context-batch32",  # max-length for 32 users
         "long-context-32k",  # max-length
-        "pcc-1L",  # pcc check for 1L
+        "apc-test",  # apc check for 80L + teacher forced for prefill + pcc check on prefill and 1st decode token
         "pcc-80L",  # pcc check for 80L + teacher forced
     ],
 )
@@ -334,7 +354,7 @@ def test_demo_text(
     stop_at_eos,
     mesh_device,
     is_ci_env,
-    ci_only,
+    apc_test,
     num_layers,
     pcc_check,
     pcc_decode_len,
@@ -349,6 +369,10 @@ def test_demo_text(
     # TODO: Remove this once all batch sizes are supported on TG
     if os.environ.get("MESH_DEVICE") == "TG" and batch_size not in [1, 32]:
         pytest.skip("Llama TG only supports batch-32")
+    if apc_test and not pcc_check:
+        raise ValueError("APC test requires PCC check to be enabled")
+    if pcc_check:
+        demo_targets = load_demo_targets("models/demos/llama3_subdevices/demo/text_demo_targets.json", galaxy_type)
 
     enable_trace = True  # Use tracing for better perf
     prefill_enable_trace = True  # repeat_batches > 1
@@ -600,11 +624,14 @@ def test_demo_text(
         if pcc_check:
             torch_output_logits = torch_output[0]
             logits = tt_out_logits_all_users[0, 0, :vocab_size]
-            does_pass, pcc_message = comp_pcc(logits, torch_output_logits, 0.91)
+            does_pass, pcc_message = comp_pcc(logits, torch_output_logits, demo_targets["prefill_pcc"])
             logger.info(f"PCC: {pcc_message}")
             logger.info(
                 f"Teacher forced token at prefill {'PASSED' if does_pass else 'FAILED'} PCC check with torch reference model"
             )
+            assert_message = f"Prefill PCC check failed: {pcc_message}, while expected {demo_targets['prefill_pcc']}. If it is expected to be different in Llama model, please update the text_demo_targets_{str(galaxy_type).lower()}.json file."
+            if apc_test:
+                assert pcc_message == demo_targets["prefill_pcc"], assert_message
 
         # Save prefill token
         prefilled_token = toks.view(-1, 1)
@@ -659,13 +686,20 @@ def test_demo_text(
             else:
                 profiler.start(f"inference_decode_time_{iteration}", iteration=batch_idx)
 
+            # Determine whether to enable trace
+            if apc_test:
+                assert pcc_check, "APC test is only supported with PCC check enabled"
+                is_enable_trace = iteration != 0  # First iteration is compile time and checks PCC
+            else:
+                is_enable_trace = enable_trace if not pcc_check else False
             # Run decode forward
             try:
-                tt_out_logits_saved = torch.zeros(vocab_size) if pcc_check else None
+                # Save logits only for PCC check when tracing is disabled
+                tt_out_logits_saved = torch.zeros(vocab_size) if (pcc_check and not is_enable_trace) else None
                 tt_out_tok = generator.decode_forward_text(
                     out_tok,
                     current_pos,
-                    enable_trace=enable_trace if not pcc_check else False,
+                    enable_trace=is_enable_trace,
                     page_table=page_table,
                     kv_cache=tt_kv_cache,
                     read_from_device=True,
@@ -685,21 +719,30 @@ def test_demo_text(
                 decode_iteration_time = profiler.get_duration(f"inference_decode_time_{iteration}", iteration=batch_idx)
 
             # If there is PCC check we perform teacher forcing, swap token with reference model (decode check only done for 80 layers)
+            # If it's apc_test we do not teacher force, but we still check PCC for only iteration == 0
             teacher_forcing = (
-                pcc_check and max_encoded_prompt_len + iteration + 1 < len(ref_tokens) and num_layers == 80
+                not apc_test
+                and pcc_check
+                and max_encoded_prompt_len + iteration + 1 < len(ref_tokens)
+                and num_layers == 80
             )
             out_tok = tt_out_tok[0] if not teacher_forcing else ref_tokens[max_encoded_prompt_len + iteration + 1]
 
             if out_tok.shape == torch.Size([]) or (len(out_tok.shape) > 0 and out_tok.shape[0] != 32):
                 out_tok = out_tok.repeat(32, 1)
 
-            if teacher_forcing:
+            if teacher_forcing or (apc_test and iteration == 0):
                 torch_output_logits = torch_output[iteration + 1]
-                does_pass, pcc_message = comp_pcc(tt_out_logits_saved, torch_output_logits, 0.91)
+                does_pass, pcc_message = comp_pcc(tt_out_logits_saved, torch_output_logits, demo_targets["decode_pcc"])
                 logger.info(f"PCC: {pcc_message}")
                 logger.info(
                     f"Teacher forced token at decode iteration {iteration} {'PASSED' if does_pass else 'FAILED'} PCC check with torch reference model"
                 )
+                assert_message = f"Decode PCC check failed: {pcc_message}, while expected {demo_targets['decode_pcc']}. If any ops in Llama model might be impacted, please update decode_pcc in the text_demo_targets_{str(galaxy_type).lower()}.json file."
+                if apc_test:
+                    assert pcc_message == demo_targets["decode_pcc"], assert_message
+
+            if teacher_forcing:
                 _, tt_top5_tokens = torch.topk(tt_out_logits_saved, k=5, dim=-1)
                 _, ref_top5_tokens = torch.topk(torch_output_logits, k=5, dim=-1)
                 top_1_acc = tt_top5_tokens[0] == ref_top5_tokens[0]
@@ -716,7 +759,12 @@ def test_demo_text(
             logger.info(
                 f"Iteration {iteration}: {1000*decode_iteration_time:.0f}ms @ {tokens_per_second_per_user:.1f} tok/s/user ({batch_size*tokens_per_second_per_user:.1f} tok/s throughput)"
             )
-
+            if apc_test and demo_targets["token_pos"] == current_pos:
+                lower_bound = demo_targets["throughput"] - demo_targets["absolute_margin"]
+                upper_bound = demo_targets["throughput"] + demo_targets["absolute_margin"]
+                assert (
+                    lower_bound <= tokens_per_second_per_user <= upper_bound
+                ), f"Throughput for APC test is not within the expected range. Current throughput: {tokens_per_second_per_user:.1f} tok/s/user, Update text_demo_targets_{str(galaxy_type).lower()}.json file with the expected throughput."
             current_pos += 1
 
             # Save output token to print out later
@@ -869,7 +917,7 @@ def test_demo_text(
         "Full demo runtime": profiler.get_duration("run"),
     }
 
-    if num_layers == 80 and pcc_check and len(top_1_accs) > 0 and len(top_5_accs) > 0:
+    if not apc_test and num_layers == 80 and pcc_check and len(top_1_accs) > 0 and len(top_5_accs) > 0:
         measurements["Top 1 Accuracy"] = sum(top_1_accs) / len(top_1_accs)
         measurements["Top 5 Accuracy"] = sum(top_5_accs) / len(top_5_accs)
 

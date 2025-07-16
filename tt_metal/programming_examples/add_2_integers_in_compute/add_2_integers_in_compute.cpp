cf56899ddaf98b08820ce40263ff1fd981ccc61f
bcheemasabirTT <bcheemasabir@tenstorrent.com>
2025-07-16 22:45:28 +0000

    applied pr feedback

--- tt_metal/programming_examples/add_2_integers_in_compute/add_2_integers_in_compute.cpp
+++ tt_metal/programming_examples/add_2_integers_in_compute/add_2_integers_in_compute.cpp
@@ -6,8 +6,6 @@
 #include <tt-metalium/device.hpp>
 #include <tt-metalium/bfloat16.hpp>
 #include <tt-metalium/distributed.hpp>
-#include <tt-metalium/tt_metal.hpp>
-#include <tt-metalium/kernel.hpp>
 
 using namespace tt;
 using namespace tt::tt_metal;
@@ -17,6 +15,9 @@ using namespace tt::tt_metal;
 int main() {
     /* Silicon accelerator setup */
 
+    //A MeshDevice is a software concept that allows developers to virtualize a cluster of connected devices as a single object,
+    // maintaining uniform memory and runtime state across all physical devices.
+    //A UnitMesh is a 1x1 MeshDevice that allows users to interface with a single physical device.
     std::shared_ptr<distributed::MeshDevice> mesh_device = distributed::MeshDevice::create_unit_mesh(
         0, DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, 1, DispatchCoreType::WORKER);
 
@@ -26,6 +27,7 @@ int main() {
     distributed::MeshCoordinateRange device_range = distributed::MeshCoordinateRange(mesh_device->shape());
     Program program = CreateProgram();
 
+    const auto device_coord = distributed::MeshCoordinate(0, 0);
     constexpr CoreCoord core = {0, 0};
 
     constexpr uint32_t single_tile_size = 2 * 1024;
@@ -53,20 +55,20 @@ int main() {
     CircularBufferConfig cb_src0_config =
         CircularBufferConfig(num_input_tiles * single_tile_size, {{src0_cb_index, tt::DataFormat::Float16_b}})
             .set_page_size(src0_cb_index, single_tile_size);
-    CBHandle cb_src0 = tt_metal::CreateCircularBuffer(program, core, cb_src0_config);
+    tt_metal::CreateCircularBuffer(program, core, cb_src0_config);
 
     constexpr uint32_t src1_cb_index = CBIndex::c_1;
     CircularBufferConfig cb_src1_config =
         CircularBufferConfig(num_input_tiles * single_tile_size, {{src1_cb_index, tt::DataFormat::Float16_b}})
             .set_page_size(src1_cb_index, single_tile_size);
-    CBHandle cb_src1 = tt_metal::CreateCircularBuffer(program, core, cb_src1_config);
+    tt_metal::CreateCircularBuffer(program, core, cb_src1_config);
 
     constexpr uint32_t output_cb_index = CBIndex::c_16;
     constexpr uint32_t num_output_tiles = 1;
     CircularBufferConfig cb_output_config =
         CircularBufferConfig(num_output_tiles * single_tile_size, {{output_cb_index, tt::DataFormat::Float16_b}})
             .set_page_size(output_cb_index, single_tile_size);
-    CBHandle cb_output = tt_metal::CreateCircularBuffer(program, core, cb_output_config);
+    tt_metal::CreateCircularBuffer(program, core, cb_output_config);
 
     /* Specify data movement kernels for reading/writing data to/from DRAM */
     KernelHandle binary_reader_kernel_id = CreateKernel(
@@ -96,17 +98,7 @@ int main() {
             .compile_args = compute_kernel_args,
         });
 
-    /* Create source data and write to DRAM */
-    std::vector<uint32_t> src0_vec;
-    std::vector<uint32_t> src1_vec;
-    src0_vec = create_constant_vector_of_bfloat16(single_tile_size, 14.0f);
-    src1_vec = create_constant_vector_of_bfloat16(single_tile_size, 8.0f);
-
-    auto coord = distributed::MeshCoordinate(0, 0);
-    distributed::WriteShard(cq, src0_dram_buffer, src0_vec, coord);
-    distributed::WriteShard(cq, src1_dram_buffer, src1_vec, coord);
-
-    /* Configure program and runtime kernel arguments, then execute */
+    /* Configure program and runtime kernel arguments, then add to workload */
     SetRuntimeArgs(
         program,
         binary_reader_kernel_id,
@@ -114,14 +106,29 @@ int main() {
         {src0_dram_buffer->address(), src1_dram_buffer->address(), src0_bank_id, src1_bank_id});
     SetRuntimeArgs(program, eltwise_binary_kernel_id, core, {});
     SetRuntimeArgs(program, unary_writer_kernel_id, core, {dst_dram_buffer->address(), dst_bank_id});
-
+    
     distributed::AddProgramToMeshWorkload(workload, std::move(program), device_range);
+    /* Create source data and write to DRAM */
+    std::vector<uint32_t> src0_vec;
+    std::vector<uint32_t> src1_vec;
+    src0_vec = create_constant_vector_of_bfloat16(single_tile_size, 14.0f);
+    src1_vec = create_constant_vector_of_bfloat16(single_tile_size, 8.0f);
+
+    //We're writing to a shard allocated on Device Coordinate 0, 0, since this is a 1x1 
+    // When the MeshDevice is 2 dimensional, this API can be used to target specific physical devices
+    distributed::WriteShard(cq, src0_dram_buffer, src0_vec, device_coord);
+    distributed::WriteShard(cq, src1_dram_buffer, src1_vec, device_coord);
+
+    /* Execute the workload */
     distributed::EnqueueMeshWorkload(cq, workload, false);
     Finish(cq);
 
     /* Read in result into a host vector */
     std::vector<uint32_t> result_vec;
-    distributed::ReadShard(cq, result_vec, dst_dram_buffer, coord);
+
+    //We're reading from a shard allocated on Device Coordinate 0, 0, since this is a 1x1 
+    // When the MeshDevice is 2 dimensional, this API can be used to target specific physical devices
+    distributed::ReadShard(cq, result_vec, dst_dram_buffer, device_coord);
 
     printf("Result = %d\n", result_vec[0]);  // 22 = 1102070192
     printf(
